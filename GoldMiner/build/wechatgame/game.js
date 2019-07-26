7c9409141589950f6194d00c0af67bd6c512a0c2
wang-sj16 <wang-sj16@mails.tsinghua.edu.cn>
2019-07-26 16:38:44 +0800

    modify assert

--- GoldMiner/build/wechatgame/game.js
+++ GoldMiner/build/wechatgame/game.js
@@ -2,9 +2,9 @@ require('libs/weapp-adapter/index');
 var Parser = require('libs/xmldom/dom-parser');
 window.DOMParser = Parser.DOMParser;
 require('libs/wx-downloader.js');
-require('src/settings.e3b43');
+require('src/settings.9e1ea');
 var settings = window._CCSettings;
-require('main.4c801');
+require('main.6757d');
 
 // Will be replaced with cocos2d-js path in editor
 require('cocos/cocos2d-js-min.js');
